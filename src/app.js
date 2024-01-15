096bfa900a1566281f9e4dcaec79a386f193b150
Legend Andrews <139176918+Cantrell24@users.noreply.github.com>
2024-01-15 15:33:58 +0000

    Finished Domain Name

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let articless = ["the", "our", "them", "they", "she"];
+  let adjectives = ["great", "big", "thrilling", "funny", "huge"];
+  let nouns = ["jogger", "racoon", "car", "spaceship", "tiger"];
+  let tlds = [".com", "org", ".io", ".gov", ".app"];
+
+  let domainNames = [];
+
+  for (let article of articless) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          domainNames.push(`${article}${adjective}${noun}${tld}`);
+        }
+      }
+    }
+  }
+  document.body.innerHTML = `<ul> ${domainNames
+    .map(domainNames => `<li>${domainNames}</li>`)
+    .join(" ")} </ul>`;
 };
+
+//articles.foreach(articles);
+
+//console.log(`${pronoun[0]}${adj[0]}${noun[0]}${tlds[0]}`);
